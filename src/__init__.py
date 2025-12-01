661ba253b76d041b5b40177bd1f5813710c65bab
Ganendra Aryaputra <kemaproject01@gmail.com>
2025-12-01 20:48:24 +0700

    init py

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,41 @@
-__all__ = ['module1', 'module2']
-__version__ = '0.1.0'
+"""
+Churn Classification MLOps Package
+
+A complete machine learning operations project for customer churn prediction
+using Docker, DVC, and CI/CD automation.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Stavanger"
+__email__ = "Aryaganendra45@gmail.com"
+
+__all__ = [
+    'config',
+    'evaluate',
+    'predict',
+    'preprocess',
+    'train',
+    'utils'
+]
+
+from src.config import Config
+from src.utils import (
+    setup_directories,
+    load_data,
+    save_data,
+    generate_sample_data
+)
+
+def get_version():
+    "Get Package Version"
+    return __version__
+
+def get_info():
+    "Get package information"
+    return {
+        'name' : 'churn-classification-mlops',
+        'version' : __version__,
+        'author' : __author__,
+        'email' : __email__,
+        'description': 'MLOps project for customer churn prediction'
+    }
